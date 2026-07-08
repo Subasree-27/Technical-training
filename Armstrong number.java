7cdb97931d55892b793dac229012e16068fb25ad
Subasree-27 <suba272006@gmail.com>
2026-07-08 16:52:32 +0530

    Add Armstrong number checker in Java

---
+++ Armstrong number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),temp=n,sum=0;
+        while(n>0){
+            int d=n%10;
+            sum=sum+d*d*d;
+            n=n/10;
+        }
+        if(sum==temp)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
